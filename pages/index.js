0935b042239c427da4d721002937d111866edf39
Sillas Vinícius Barbosa Braga <sillasvinicius0403@gmail.com>
2025-04-21 17:31:20 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>O homem bom traz coisas boas do bem que carrega em seu coração, e o homem mau revela coisas malignas da maldade que carrega em seu coração.</h1>
+}
+
+export default Home;
