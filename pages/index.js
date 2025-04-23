21b038c8ebb9f12af5beb6ce4aa2234fa72519ca
Sillas Vinícius Barbosa Braga <sillasvinicius0403@gmail.com>
2025-04-23 23:12:04 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>O homem bom traz coisas boas do bem que carrega em seu coração, e o homem mau revela coisas malignas da maldade que carrega em seu coração. ✌️</h1>
+  return (
+    <h1>
+      O homem bom traz coisas boas do bem que carrega em seu coração, e o homem
+      mau revela coisas malignas da maldade que carrega em seu coração. ✌️
+    </h1>
+  );
 }
 
 export default Home;
