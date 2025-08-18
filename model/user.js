4e31c0a25987818abb5734bf51e368a77241524a
sillas.braga <sillas.braga@sankhya.com>
2025-08-18 16:01:01 -0300

    feat: cria método `user.findOneByUsername` e endPoint `/api/v1/users/[username]`

--- model/user.js
+++ model/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { NotFoundError, ValidationError } from "infra/errors";
 
 async function runInsertQuery({ username, email, password }) {
   const results = await database.query({
@@ -37,6 +37,27 @@ async function validateUniqueUsername(username) {
   }
 }
 
+async function runSelectQuery(username) {
+  const results = await database.query({
+    text: "SELECT * FROM users WHERE LOWER(username) = LOWER($1) LIMIT 1 ;",
+    values: [username],
+  });
+
+  if (results?.rowCount === 0) {
+    throw new NotFoundError({
+      message: "O nome de usuário informado não foi encontrado no sistema.",
+      action: "Verifique se o nome de usuário está digitado corretamente.",
+    });
+  }
+
+  return results.rows[0];
+}
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+}
+
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues?.email);
   await validateUniqueUsername(userInputValues?.username);
@@ -46,6 +67,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
