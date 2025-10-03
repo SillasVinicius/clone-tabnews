5c55b33c791b88b4ceb61a5cd1c56d73f91c954a
sillas.braga <sillas.braga@sankhya.com>
2025-10-03 16:36:47 -0300

    feat: limpa cookie de sessão ao utilizar sessão expirada

--- tests/integration/api/v1/sessions/delete.test.js
+++ tests/integration/api/v1/sessions/delete.test.js
@@ -32,6 +32,19 @@ describe("DELETE /api/v1/sessions", () => {
         action: "verifique se este usuário está logado e tente novamente.",
         status_code: 401,
       });
+
+      // Set-Cookie assertions
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: "invalid",
+        maxAge: -1,
+        path: "/",
+        httpOnly: true,
+      });
     });
 
     test("Sessão expirada", async () => {
@@ -61,6 +74,19 @@ describe("DELETE /api/v1/sessions", () => {
         message: "O usuário não possui sessão ativa.",
         action: "verifique se este usuário está logado e tente novamente.",
         status_code: 401,
+      });
+
+      // Set-Cookie assertions
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: "invalid",
+        maxAge: -1,
+        path: "/",
+        httpOnly: true,
       });
     });
 
